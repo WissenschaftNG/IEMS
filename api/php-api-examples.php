eec19653a663a4d31456e8adb44d7df69c7bbd52
Ezeasor Ekene <ezeasorekene@gmail.com>
2018-07-26 21:11:18 +0100

    Minor fix

--- api/php-api-examples.php
+++ api/php-api-examples.php
@@ -11,9 +11,9 @@ print iems_api_response('faculties');
 print iems_api_response('staff');
 
 //To print the surname of the second staff in the staff list
-print iems_api_response($data_type,TRUE,1,'Surname');
+print iems_api_response('staff',TRUE,1,'Surname');
 
 //To print the surname of a particular staff using the iEMS ID-2018001
-print iems_api_response($data_type,TRUE,0,'Surname','2018001');
+print iems_api_response('staff',TRUE,0,'Surname','2018001');
 
 ?>

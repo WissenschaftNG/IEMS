ad874db6423b440850c8ed62c762f7cbe3074c67
Ekene Ezeasor <ezeasorekene@users.noreply.github.com>
2018-07-26 01:57:24 +0100

    Update php-api-response.php

--- php-api-response.php
+++ php-api-response.php
@@ -16,7 +16,7 @@ $fetch_type = "API-FETCH-TYPE"; //Default is 'all'. Available are all and {Staff
 //Get the iEMS API authentication key
 if (!function_exists('iems_api_auth'){
   function iems_api_auth($api_key){
-	  return iems_api_url().$api_key.'/';
+	return iems_api_url().$api_key.'/';
   }
 }
 
@@ -24,12 +24,12 @@ if (!function_exists('iems_api_auth'){
 //Get the iEMS API URL
 if (!function_exists('iems_api_url'){
   function iems_api_url($api_url){
-	  if(substr($api_url,-1)!=='/'){
-		  // add ending '/' if not exists
-		  $api_url = $api_url.'/'.iems_api_auth();
-	  }
-	  // returns the url with an ending single slash '/'
-	  return $api_url.'/';
+	if(substr($api_url,-1)!=='/'){
+	  // add ending '/' if not exists
+	  $api_url = $api_url.'/'.iems_api_auth();
+	}
+	// returns the url with an ending single slash '/'
+	return $api_url.'/';
   }
 }
 
@@ -37,12 +37,12 @@ if (!function_exists('iems_api_url'){
 //Set the api call, data and fetch types to get a successful response
 if (!function_exists('iems_api_response'){
   function iems_api_response($row,$response,$data_type,$call_type='get',$fetch_type='all'){
-	  //Calls sent to the api are get calls requesting all listed data by default
-	  $api_url = iems_api_url().$call_type.'/'.$data_type.'/'.$fetch_type;
-	  $json = file_get_contents($api_url);
-	  $data = json_decode($json, TRUE);
-	  $api_response = $data[$row][$response];
-	  return $api_response;
+	//Calls sent to the api are get calls requesting all listed data by default
+	$api_url = iems_api_url().$call_type.'/'.$data_type.'/'.$fetch_type;
+	$json = file_get_contents($api_url);
+	$data = json_decode($json, TRUE);
+	$api_response = $data[$row][$response];
+	return $api_response;
   }
 }
 

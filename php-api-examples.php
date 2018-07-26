13b735d1350e3c29d19746eec1c93b309a189e06
Ezeasor Ekene <ezeasorekene@gmail.com>
2018-07-26 19:04:10 +0100

    Updated live examples

---
+++ php-api-examples.php
@@ -0,0 +1,19 @@
+<?php
+require 'php-api-response.php';
+
+//To print all departments
+print iems_api_response('departments');
+
+//To print all faculties
+print iems_api_response('faculties');
+
+//To print all staff list
+print iems_api_response('staff');
+
+//To print the surname of the second staff in the staff list
+print iems_api_response($data_type,TRUE,1,'Surname');
+
+//To print the surname of a particular staff using the iEMS ID-2018001
+print iems_api_response($data_type,TRUE,0,'Surname','2018001');
+
+?>
